69e992244b944c85cfc3870cfdd35aa263b22534
OLDPENG <13397396958@163.com>
2016-06-13 17:19:19 +0800

    1

--- app/src/main/java/com/yuxuan/flatshare/Activity/SplashActivity.java
+++ app/src/main/java/com/yuxuan/flatshare/Activity/SplashActivity.java
@@ -23,33 +23,36 @@ public class SplashActivity extends AppCompatActivity {
     private void initView() {
         setContentView(R.layout.activity_splash);
 
+        //dddddddddddddddddddd
+
         ivSplash = (ImageView) findViewById(R.id.ivSplash);
         //自定义透明动画
         AlphaAnimation alphaAnimation = new AlphaAnimation(0f, 1f);
-        alphaAnimation.setDuration(1500);
+        alphaAnimation.setDuration(2000);
         alphaAnimation.setFillAfter(true);
         //给图片设置透明动画
         ivSplash.setAnimation(alphaAnimation);
 
-        //1.5秒后才跳转下一个页面
+        //2秒后才跳转下一个页面
         new Thread(new Runnable() {
             @Override
             public void run() {
-                SystemClock.sleep(1500);
+                //暂停2秒
+                SystemClock.sleep(2000);
+
+                Intent intent = new Intent();
+                //判断第几次使用
+                if (ConfigUtils.getIsRun(SplashActivity.this)) {
+                    //第一次使用APP
+                    intent.setClass(SplashActivity.this, GuideActivity.class);
+                } else {
+                    //已经使用过APP
+                    intent.setClass(SplashActivity.this, MainActivity.class);
+                }
+                //跳转页面并关闭当前页面
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
             }
         }).start();
-
-        Intent intent = new Intent();
-        //判断第几次使用
-        if (ConfigUtils.getIsRun(this)) {
-            //第一次使用APP
-            intent.setClass(this, GuideActivity.class);
-        } else {
-            //已经使用过APP
-            intent.setClass(this, MainActivity.class);
-        }
-        //跳转页面并关闭当前页面
-        this.startActivity(intent);
-        this.finish();
     }
 }
